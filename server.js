04e73f5737514bf8c43545b52e7493c5e6c3038b
ZhiruiJiang98 <jiangzrui9@gmail.com>
2022-08-18 17:45:11 -0400

    server.js change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(res, req){
-    res.setEncoding("WORKING!!!");
+    res.send("WORKING!!!");
 });
 
 app.listen(process.env.PORT||3000)
