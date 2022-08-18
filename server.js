d0e561b85c35c4633523accad17bc233f83aaccc
ZhiruiJiang98 <jiangzrui9@gmail.com>
2022-08-18 18:05:27 -0400

    new package-lock.json

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express')
+const cors = require('cors');
+const axios = require('axios');
+
 const app = express();
 
-app.get("/", function(res, req){
-    res.send("WORKING!!!");
-});
+app.use(cors());
 
-app.listen(process.env.PORT||3000)
+const PORT = 4000;
+app.list(PORT, () => console.log('Listening on PORT', PORT))
