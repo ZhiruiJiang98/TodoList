a53427674958c74339eaab70de8db1133ce4fa65
ZhiruiJiang98 <jiangzrui9@gmail.com>
2022-08-18 15:35:24 -0400

    package withouth package-lock.json

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 const PATH = require('path')
-const PORT = process.env.PORT || 5555
+const PORT = process.env.PORT || 3000
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'))
   app.get('*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    req.sendFile(PATH.resolve(__dirname, 'build', 'index.html'))
   })
 }
 
-app.listen(port, err => {
+app.listen(PORT, err => {
   if (err) return console.log(err)
   console.log('Server running on port:', PORT)
 })
