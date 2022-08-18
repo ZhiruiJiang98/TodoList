551c6dd657df2e6f37c52b090f6f71b0b19a6961
ZhiruiJiang98 <jiangzrui9@gmail.com>
2022-08-18 17:08:13 -0400

    change new server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,8 @@
-require("dotenv").config()
-import express from "express";
-import configViewEngine from "./config/viewEngine"
-import initWebRoutes from "./routes/web";
-import bodyParser from "body-parser"
-import initCronJob from "./config/cronJob"
+const express = require("express");
+const app = express();
 
-let app = express()
+app.get("/", function(res, req){
+    res.setEncoding("WORKING!!!");
+});
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extends:true}));
-
-configViewEngine(app);
-
-initWebRoutes(app);
-
-initCronJob();
-
-let port = process.env.PORT||3000;
-
-app.listen(port,() => {
-    console.log("app is running at the port". port);
-})
+app.listen(process.env.PORT||5000)
